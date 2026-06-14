8a1b51853d44adf1e7b22b23f860f164b6754089
paras-7span <paras@7span.com>
2026-06-14 16:56:06 +0530

    [Fix]: Routing issue

--- app/components/car/card.vue
+++ app/components/car/card.vue
@@ -47,7 +47,7 @@
             <div class="flex items-center justify-between gap-2">
                 <UButton :to="`/cars/${car.id}`" label="View Details"
                     class=" justify-center  w-full active:bg-gray-900 bg-gray-900 text-white hover:bg-gray-950 " />
-                <UButton :to="`/compare`" label="Compare Cars"
+                <UButton :to="`cars/compare`" label="Compare Cars"
                     class=" justify-center  w-full active:bg-gray-900 bg-gray-900 text-white hover:bg-gray-950 " />
             </div>
 
